e269eba9d48299ac0273ea43f44efb83e2a4a593
Rodrigovicente <rodrigovcalabria@hotmail.com>
2024-10-17 22:51:45 -0300

    dashboard API integration

--- src/app/(logged-area)/dashboard/page.tsx
+++ src/app/(logged-area)/dashboard/page.tsx
@@ -2,45 +2,80 @@ import { Input } from '@/components/ui/input'
 import DpsDataTable, { DPS } from '../components/dps-data-table'
 import { ListFilterIcon, SearchIcon } from 'lucide-react'
 import { Button } from '@/components/ui/button'
+import { getProposals } from './actions'
+import { authOptions } from '@/app/api/auth/[...nextauth]/auth-options'
+import { getServerSession } from 'next-auth'
+import { redirect } from 'next/navigation'
 
-export default function DashboardPage() {
-	const data: DPS[] = [
-		{
-			codigo: 'INV-10022024-001',
-			cpf: '875.896.857-89',
-			dataCadastro: new Date('2024-09-15T00:00:00-03:00'),
-			tipoDoc: 'simples',
-			status: 'assinada',
-		},
-		{
-			codigo: 'INV-10022024-002',
-			cpf: '123.252.548-89',
-			dataCadastro: new Date('2024-09-15T00:00:00-03:00'),
-			tipoDoc: 'simples',
-			status: 'analise',
-		},
-		{
-			codigo: 'INV-10022024-003',
-			cpf: '123.252.857-89',
-			dataCadastro: new Date('2024-08-11T00:00:00-03:00'),
-			tipoDoc: 'completa',
-			status: 'aceita',
-		},
-		{
-			codigo: 'INV-10022024-004',
-			cpf: '000.252.548-89',
-			dataCadastro: new Date('2024-05-01T00:00:00-03:00'),
-			tipoDoc: 'completa',
-			status: 'anexar',
-		},
-		{
-			codigo: 'INV-10022024-009',
-			cpf: '123.087.548-89',
-			dataCadastro: new Date('2024-10-15T00:00:00-03:00'),
-			tipoDoc: 'completa',
-			status: 'pendente',
-		},
-	]
+export default async function DashboardPage({
+	searchParams,
+}: {
+	searchParams: { page: string }
+}) {
+	const session = await getServerSession(authOptions)
+	const token = (session as any)?.accessToken
+
+	const currentPage = searchParams?.page ? +searchParams.page : 1
+
+	const data = await getProposals(token, currentPage)
+	console.log('||||||||->>', data?.items)
+
+	const pageAmount = Math.ceil(data?.totalItems / 10)
+
+	if (
+		data === null ||
+		(searchParams?.page && +searchParams.page > pageAmount)
+	) {
+		return redirect('/dashboard')
+	}
+
+	const tableRowsData: DPS[] = data?.items.map((item: any) => {
+		return {
+			codigo: item.uid,
+			cpf: item.customer.document,
+			dataCadastro: item?.createdAt && new Date(item.createdAt),
+			tipoDoc: item.type?.description,
+			status: item.status,
+		}
+	})
+
+	// const data: DPS[] = [
+	// 	{
+	// 		codigo: 'INV-10022024-001',
+	// 		cpf: '875.896.857-89',
+	// 		dataCadastro: new Date('2024-09-15T00:00:00-03:00'),
+	// 		tipoDoc: 'simples',
+	// 		status: 'assinada',
+	// 	},
+	// 	{
+	// 		codigo: 'INV-10022024-002',
+	// 		cpf: '123.252.548-89',
+	// 		dataCadastro: new Date('2024-09-15T00:00:00-03:00'),
+	// 		tipoDoc: 'simples',
+	// 		status: 'analise',
+	// 	},
+	// 	{
+	// 		codigo: 'INV-10022024-003',
+	// 		cpf: '123.252.857-89',
+	// 		dataCadastro: new Date('2024-08-11T00:00:00-03:00'),
+	// 		tipoDoc: 'completa',
+	// 		status: 'aceita',
+	// 	},
+	// 	{
+	// 		codigo: 'INV-10022024-004',
+	// 		cpf: '000.252.548-89',
+	// 		dataCadastro: new Date('2024-05-01T00:00:00-03:00'),
+	// 		tipoDoc: 'completa',
+	// 		status: 'anexar',
+	// 	},
+	// 	{
+	// 		codigo: 'INV-10022024-009',
+	// 		cpf: '123.087.548-89',
+	// 		dataCadastro: new Date('2024-10-15T00:00:00-03:00'),
+	// 		tipoDoc: 'completa',
+	// 		status: 'pendente',
+	// 	},
+	// ]
 
 	return (
 		<div className="p-5">
@@ -59,7 +94,11 @@ export default function DashboardPage() {
 						<ListFilterIcon size={20} />
 					</Button>
 				</div>
-				<DpsDataTable data={data} />
+				<DpsDataTable
+					data={tableRowsData}
+					currentPage={currentPage}
+					pageAmount={pageAmount}
+				/>
 			</div>
 		</div>
 	)
--- src/components/ui/data-table.tsx
+++ src/components/ui/data-table.tsx
@@ -31,11 +31,19 @@ import {
 interface DataTableProps<TData, TValue> {
 	columns: ColumnDef<TData, TValue>[]
 	data: TData[]
+	currentPage?: number
+	pageAmount?: number
+	getPageUrl?: (page: number) => string
+	onPageChange?: (page: number) => void
 }
 
 export function DataTable<TData, TValue>({
 	columns,
 	data,
+	currentPage,
+	pageAmount,
+	getPageUrl,
+	onPageChange,
 }: DataTableProps<TData, TValue>) {
 	const table = useReactTable({
 		data,
@@ -85,49 +93,208 @@ export function DataTable<TData, TValue>({
 					</TableRow>
 				)}
 			</TableBody>
-			<TableFooter>
-				<TableRow>
-					<TableCell colSpan={columns.length}>
-						<TablePagination />
-					</TableCell>
-				</TableRow>
-			</TableFooter>
+			{pageAmount && currentPage ? (
+				<TableFooter>
+					<TableRow>
+						<TableCell colSpan={columns.length}>
+							<TablePagination
+								currentPage={currentPage}
+								pageAmount={pageAmount}
+								getPageUrl={getPageUrl}
+								onPageChange={onPageChange}
+							/>
+						</TableCell>
+					</TableRow>
+				</TableFooter>
+			) : null}
 		</Table>
 	)
 }
 
-export function TablePagination() {
+export function TablePagination({
+	currentPage,
+	pageAmount,
+	getPageUrl,
+	onPageChange,
+}: {
+	currentPage: number
+	pageAmount: number
+	getPageUrl?: (page: number) => string
+	onPageChange?: (page: number) => void
+}) {
+	if (pageAmount <= 1) {
+		return null
+	}
+
+	let pageButtons: JSX.Element[] = []
+
+	if (pageAmount <= 5) {
+		pageButtons = Array.from({ length: pageAmount }, (_, i) => (
+			<PaginationItem key={i}>
+				<PaginationLink
+					onClick={() => onPageChange?.(i + 1)}
+					href={getPageUrl?.(i + 1)}
+					isActive={currentPage === i + 1}
+				>
+					{i + 1}
+				</PaginationLink>
+			</PaginationItem>
+		))
+	} else {
+		if (currentPage <= 3) {
+			pageButtons = Array.from({ length: 3 }, (_, i) => (
+				<PaginationItem key={i}>
+					<PaginationLink
+						onClick={() => onPageChange?.(i + 1)}
+						href={getPageUrl?.(i + 1)}
+						isActive={currentPage === i + 1}
+					>
+						{i + 1}
+					</PaginationLink>
+				</PaginationItem>
+			))
+
+			pageButtons.push(
+				<PaginationItem>
+					<PaginationEllipsis />
+				</PaginationItem>
+			)
+
+			pageButtons.push(
+				<PaginationItem>
+					<PaginationLink
+						href={getPageUrl?.(pageAmount)}
+						onClick={() => onPageChange?.(pageAmount)}
+					>
+						{pageAmount}
+					</PaginationLink>
+				</PaginationItem>
+			)
+		} else if (currentPage > pageAmount - 3) {
+			pageButtons.push(
+				<PaginationItem>
+					<PaginationLink
+						href={getPageUrl?.(1)}
+						onClick={() => onPageChange?.(1)}
+					>
+						1
+					</PaginationLink>
+				</PaginationItem>
+			)
+
+			pageButtons.push(
+				<PaginationItem>
+					<PaginationEllipsis />
+				</PaginationItem>
+			)
+
+			pageButtons.push(
+				...Array.from({ length: 3 }, (_, i) => {
+					const page = pageAmount - 2 + i
+					return (
+						<PaginationItem key={i}>
+							<PaginationLink
+								onClick={() => onPageChange?.(page)}
+								href={getPageUrl?.(page)}
+								isActive={currentPage === page}
+							>
+								{page}
+							</PaginationLink>
+						</PaginationItem>
+					)
+				})
+			)
+		} else {
+			pageButtons.push(
+				<PaginationItem>
+					<PaginationLink
+						href={getPageUrl?.(1)}
+						onClick={() => onPageChange?.(1)}
+					>
+						1
+					</PaginationLink>
+				</PaginationItem>
+			)
+
+			pageButtons.push(
+				<PaginationItem>
+					<PaginationEllipsis />
+				</PaginationItem>
+			)
+
+			pageButtons.push(
+				...Array.from({ length: 3 }, (_, i) => {
+					const page = currentPage - 1 + i
+					return (
+						<PaginationItem key={i}>
+							<PaginationLink
+								onClick={() => onPageChange?.(page)}
+								href={getPageUrl?.(page)}
+								isActive={currentPage === page}
+							>
+								{page}
+							</PaginationLink>
+						</PaginationItem>
+					)
+				})
+			)
+
+			pageButtons.push(
+				<PaginationItem>
+					<PaginationEllipsis />
+				</PaginationItem>
+			)
+
+			pageButtons.push(
+				<PaginationItem>
+					<PaginationLink
+						href={getPageUrl?.(pageAmount)}
+						onClick={() => onPageChange?.(pageAmount)}
+					>
+						{pageAmount}
+					</PaginationLink>
+				</PaginationItem>
+			)
+		}
+	}
+
 	return (
 		<Pagination className="w-full flex justify-end">
 			<PaginationContent>
 				<PaginationItem>
-					<PaginationFirst href="#" />
+					<PaginationFirst
+						href={getPageUrl?.(1)}
+						disabled={currentPage === 1}
+					/>
 				</PaginationItem>
 				<PaginationItem>
-					<PaginationPrevious href="#" />
+					<PaginationPrevious
+						href={getPageUrl?.(currentPage - 1)}
+						disabled={currentPage === 1}
+					/>
 				</PaginationItem>
-				<PaginationItem>
-					<PaginationLink href="#" isActive>
-						1
-					</PaginationLink>
-				</PaginationItem>
-				<PaginationItem>
-					<PaginationLink href="#">2</PaginationLink>
-				</PaginationItem>
-				<PaginationItem>
-					<PaginationLink href="#">3</PaginationLink>
-				</PaginationItem>
-				<PaginationItem>
+
+				{pageButtons}
+
+				{/* <PaginationItem>
 					<PaginationEllipsis />
 				</PaginationItem>
+
 				<PaginationItem>
 					<PaginationLink href="#">10</PaginationLink>
+				</PaginationItem> */}
+
+				<PaginationItem>
+					<PaginationNext
+						href={getPageUrl?.(currentPage + 1)}
+						disabled={currentPage === pageAmount}
+					/>
 				</PaginationItem>
 				<PaginationItem>
-					<PaginationNext href="#" />
-				</PaginationItem>
-				<PaginationItem>
-					<PaginationLast href="#" />
+					<PaginationLast
+						href={getPageUrl?.(pageAmount)}
+						disabled={currentPage === pageAmount}
+					/>
 				</PaginationItem>
 			</PaginationContent>
 		</Pagination>
